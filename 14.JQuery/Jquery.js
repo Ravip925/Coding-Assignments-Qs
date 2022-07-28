02d65f44aa9631ebcff8d53df209c2a6d71ee706
“Ravi <patilravi400@gmail.com>
2022-07-28 12:25:43 +0530

    js

---
+++ 14.JQuery/Jquery.js
@@ -0,0 +1,22 @@
+// Create a button , on click of which you should be able to hide a paragraph text,
+// create another button onclick of which this paragraph should be visible
+
+$("#btn1").click(function(){
+    $("p").hide();
+  
+})
+$("#btn2").click(function(){
+    $("p").show();
+  
+})
+  
+  // Create an ajax call, by using this api "https://jsonplaceholder.typicode.com/todos/12" and print the title as the heading of the page
+  
+  $.ajax({
+    url: "https://jsonplaceholder.typicode.com/todos/2",
+    type: "GET",
+    success: function (data) {
+        console.log(data);
+        document.getElementsByTagName("h1")[0].innerHTML = data.title;
+    },
+  });
